411a388b3ac79050d30d23b24d5b81bc22a1adc1
tranphuc269 <phuc260900@gmail.com>
2022-11-19 22:39:35 +0700

    update ci/cd

--- lib/data/models/response/catalog/product/cart/cart_item_data.dart
+++ lib/data/models/response/catalog/product/cart/cart_item_data.dart
@@ -1,22 +1,28 @@
 class CartItemData {
-  CartItemData({required this.productName,
+  CartItemData({
+    required this.productName,
     required this.productId,
     required this.quantity,
     required this.cartItemId,
     required this.createdAt,
     required this.extendedPrice,
     required this.itemPrice,
-    required this.updatedAt});
+    required this.updatedAt,
+    required this.images,
+  });
 
-  factory CartItemData.fromJson(Map<String, dynamic> json){
-    return CartItemData(productName: json['productName'],
-        productId: json['productId'],
-        quantity: json['quantity'],
-        cartItemId: json['cartItemId'],
-        createdAt: json['createdAt'],
-        extendedPrice: json['extendedPrice'],
-        itemPrice: json['itemPrice'],
-        updatedAt: json['updatedAt']);
+  factory CartItemData.fromJson(Map<String, dynamic> json) {
+    return CartItemData(
+      productName: json['productName'],
+      productId: json['productId'],
+      quantity: json['quantity'],
+      cartItemId: json['cartItemId'],
+      createdAt: json['createdAt'],
+      extendedPrice: json['extendedPrice'],
+      itemPrice: json['itemPrice'],
+      updatedAt: json['updatedAt'],
+      images: (json['images'] as List).map((e) => e.toString()).toList(),
+    );
   }
 
   String createdAt;
@@ -27,4 +33,5 @@ class CartItemData {
   double extendedPrice;
   String productId;
   String productName;
+  List<String> images;
 }
